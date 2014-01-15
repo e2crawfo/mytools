de653ca16d6472fc9c206b43cb63831308139c38
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-01-15 13:32:32 -0500

    make_filename more robust to input

--- file_helpers.py
+++ file_helpers.py
@@ -1,21 +1,37 @@
 import datetime
 import sys
 import os
+import string
 
-def make_plot_filename(main_title, directory='.', config_dict={}, use_date=True, sep='_'):
+def make_plot_filename(main_title, directory='.', config_dict={}, use_time=True, sep='_', extension=''):
     """
-    param main_title: the main title for the file
-    type string:
+    :param main_title: the main title for the file
+    :type string:
 
-    config_dict: maps labels to values
+    :param directory: the directory to write the file to
+    :type string:
+
+    :param config_dict: maps labels to values
+    :type dict:
+
+    :param use_time: whether to append the current date/time to the filename
+    :type bool:
+
+    :param sep: string separating items in the config dict in the returned filename
+    :param string:
+
+    :param extension: string to appear at end of filename
+    :param string:
     """
 
-    labels = [main_title]
+    if directory[-1] != '/': directory += '/'
+
+    labels = [directory + main_title]
     for label, value in config_dict.iteritems():
-        labels.append(label)
+        labels.append(str(label))
         labels.append(str(value))
 
-    if use_date:
+    if use_time:
         date_time_string = str(datetime.datetime.now()).split('.')[0]
         date_time_string = reduce(lambda y,z: string.replace(y,z,"_"), [date_time_string,":", " ","-"])
         labels.append(date_time_string)
@@ -23,7 +39,13 @@ def make_plot_filename(main_title, directory='.', config_dict={}, use_date=True,
     if not os.path.isdir(directory):
         os.makedirs(directory)
 
-    return sep.join(labels)
+    file_name = sep.join(labels)
+
+    if extension:
+        if extension[0] != '.': extension = '.' + extension
+        file_name += extension
+
+    return file_name
 
 
 
