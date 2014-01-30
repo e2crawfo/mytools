7aaf27eaefe27514130c02d4fbe11c961e68b2b9
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-01-29 20:37:34 -0500

    Exporting new plot.py functions from package.

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@
 from . import hrr
 from . import noise_functions as nf
 from . import file_helpers as fh
-from .plot import nengo_stack_plot
+from .plot import nengo_stack_plot, extract_probe_data, nengo_plot_helper
