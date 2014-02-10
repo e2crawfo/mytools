c8dccc579117260543ab487bb2dc7400491610d8
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-02-10 17:55:14 -0500

    cleanup_utilities added to __init__.py as cu

--- __init__.py
+++ __init__.py
@@ -2,4 +2,5 @@
 from . import hrr
 from . import noise_functions as nf
 from . import file_helpers as fh
+from . import cleanup_utilities as cu
 from .plot import nengo_stack_plot, extract_probe_data, nengo_plot_helper, spike_sorter, apply_funcs
