ebff3909b556c01e4ea9ed45effe2b8e3d31c725
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-06-17 15:36:34 -0400

    Updated __init__.py with dist.

--- mytools/__init__.py
+++ mytools/__init__.py
@@ -3,4 +3,5 @@ from . import hrr
 from . import noise_functions as nf
 from . import file_helpers as fh
 from . import cleanup_utilities as cu
+from . import distributions as dist
 from .plot import nengo_stack_plot, extract_probe_data, nengo_plot_helper, spike_sorter, apply_funcs
