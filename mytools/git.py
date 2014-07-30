2d9c0270533dab6cb6a9f550b108a718e074919d
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-07-30 11:33:39 -0400

    Added checks for git process calls.

--- mytools/git.py
+++ mytools/git.py
@@ -2,8 +2,18 @@ import subprocess
 
 
 def get_git_revision_hash():
-    return subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+    try:
+        h = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+    except subprocess.CalledProcessError:
+        h = None
+
+    return h
 
 
 def get_git_revision_short_hash():
-    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+    try:
+        h = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+    except subprocess.CalledProcessError:
+        h = None
+
+    return h
