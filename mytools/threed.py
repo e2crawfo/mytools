0efb4d40d67619895d4cbb180a59d53e372c8a74
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-07-30 11:36:11 -0400

    Made make_cap actually work.

--- mytools/threed.py
+++ mytools/threed.py
@@ -10,7 +10,7 @@ class Arrow3D(FancyArrowPatch):
 
     To draw a vector pointing at [1, 1, 1];
     a = Arrow3D(
-        [0,1],[0,1],[0,1], mutation_scale=20, lw=1,
+        [0, 1], [0, 1], [0, 1], mutation_scale=20, lw=1,
         arrowstyle="-|>", color="k")
     ax.add_artist(a)
     """
@@ -35,9 +35,13 @@ def rotation_matrix(axis, theta):
                      [2*(b*d-a*c), 2*(c*d+a*b), a*a+d*d-b*b-c*c]])
 
 
-def make_cap(r=1.0, cap_angle=0.33 * np.pi, direction=None):
-    u = np.linspace(0, 2 * np.pi, 100)
-    v = np.linspace(0, cap_angle, 100)
+def make_cap(r=1.0, cap_angle=0.33 * np.pi, direction=None, usteps=100, vsteps=100):
+
+    if direction is not None:
+        assert np.linalg.norm(direction) > 0
+
+    u = np.linspace(0, 2 * np.pi, usteps)
+    v = np.linspace(0, cap_angle, vsteps)
 
     x = r * np.outer(np.cos(u), np.sin(v))
     y = r * np.outer(np.sin(u), np.sin(v))
@@ -45,23 +49,25 @@ def make_cap(r=1.0, cap_angle=0.33 * np.pi, direction=None):
 
     if direction is not None:
         north = np.array([0, 0, 1])
-
-        axis = np.cross(north, direction)
         theta = np.arccos(np.dot(north, direction / np.linalg.norm(direction)))
-        print theta
 
-        R = rotation_matrix(axis, -theta)
+        if theta > 0:
+            axis = np.cross(north, direction)
 
-        x = np.reshape(x, (1, x.size))
-        y = np.reshape(y, (1, y.size))
-        z = np.reshape(z, (1, z.size))
+            R = rotation_matrix(axis, -theta)
 
-        points = np.concatenate((x, y, z), axis=0)
-        points = np.dot(R, points)
+            shape = x.shape
 
-        x = np.reshape(points[0, :], (100, 100))
-        y = np.reshape(points[1, :], (100, 100))
-        z = np.reshape(points[2, :], (100, 100))
+            x = np.reshape(x, (1, x.size))
+            y = np.reshape(y, (1, y.size))
+            z = np.reshape(z, (1, z.size))
+
+            points = np.concatenate((x, y, z), axis=0)
+            points = np.dot(R, points)
+
+            x = np.reshape(points[0, :], shape)
+            y = np.reshape(points[1, :], shape)
+            z = np.reshape(points[2, :], shape)
 
     return x, y, z
 
