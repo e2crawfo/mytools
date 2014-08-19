72481030ad0e5159886a7942999d8d15dc395024
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-08-19 18:20:41 -0400

    Added unitary arg to hrr constructor.

--- mytools/hrr.py
+++ mytools/hrr.py
@@ -8,24 +8,32 @@ def circconv(a, b):
     return np.real(ifft(fft(a)*fft(b)))
 
 class HRR:
-    def __init__(self,N=None,data=None):
+    def __init__(self, N=None, data=None, unitary=False):
         if data is not None:
             self.v=array(data)
         elif N is not None:
             self.randomize(N)
         else:
             raise Exception('Must specify size or data for HRR')
+
+        if unitary:
+            self.make_unitary()
+
     def length(self):
-        return norm(self.v)        
+        return norm(self.v)
+
     def normalize(self):
         nrm=norm(self.v)
         if nrm>0: self.v/=nrm
+
     def __str__(self):
         return str(self.v)
+
     def randomize(self,N=None):
         if N is None: N=len(self.v)
         self.v=array([random.gauss(0,1) for i in range(N)])
         self.normalize()
+
     def make_unitary(self):
         fft_val = np.fft.fft(self.v)
         fft_imag = fft_val.imag
@@ -33,63 +41,80 @@ class HRR:
         fft_norms = [sqrt(fft_imag[n]**2 + fft_real[n]**2) for n in range(len(self.v))]
         fft_unit = fft_val / fft_norms
         self.v = (np.fft.ifft(fft_unit)).real
+
     def __add__(self,other):
         return HRR(data=self.v+other.v)
+
     def __iadd__(self,other):
         self.v+=other.v
         return self
+
     def __neg__(self):
-        return HRR(data=-self.v)    
+        return HRR(data=-self.v)
+
     def __sub__(self,other):
         return HRR(data=self.v-other.v)
+
     def __isub__(self,other):
         self.v-=other.v
         return self
+
     def __mul__(self,other):
         if isinstance(other,HRR):
             return self.convolve(other)
         else:
             return HRR(data=self.v*other)
+
     def convolve(self,other):
         x=circconv(self.v,other.v)
         #x=ifft(fft(self.v)*fft(other.v)).real
         return HRR(data=x)
+
     def __rmul__(self,other):
         if isinstance(other,HRR):
             return self.convolve(other)
         else:
             return HRR(data=self.v*other)
+
     def __imul__(self,other):
         self.v=circconv(self.v,other.v)
         #self.v=ifft(fft(self.v)*fft(other.v))
         return self
+
     def compare(self,other):
         scale=norm(self.v)*norm(other.v)
         if scale==0: return 0
         return dot(self.v,other.v)/(scale)
+
     def dot(self,other):
         return dot(self.v,other.v)
+
     def distance(self,other):
         return 1-self.compare(other)
+
     def __invert__(self):
         N=len(self.v)
         return HRR(data=[self.v[0]]+[self.v[N-i] for i in range(1,N)])
+
     def __len__(self):
         return len(self.v)
+
     def copy(self):
         return HRR(data=self.v)
+
     def mse(self,other):
         err=0
         for i in range(len(self.v)):
             err+=(self.v[i]-other.v[i])**2
         return err/len(self.v)
+
     def get_transform_matrix(self):
         D=len(self.v)
         T=[]
         for i in range(D):
             T.append([self.v[(i-j)%D] for j in range(D)])
         return np.array(T)
-    
+
 
 
                        
